0c2720ed742feaba18b3f529cab687bb98cf8125
root <root@Lukas-beast.localdomain>
2021-03-02 12:29:28 +0100

    working on filter method

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const filterItems = document.querySelectorAll(".skills__item");
+const jobListing = document.querySelectorAll(".job-listing")
+
 const items = Array.from(filterItems) 
+const jobListingItems = Array.from(jobListing);
 
-console.log(items[11].dataset.languages);
+const selected = []
 
+// function filtered(){
+//   jobListingItems.filter((job)=>{
+ 
+//   })
+// }
 
+ // nie wiem jak wybrać elementy które nie są w selected arr
+  // żeby dodać klasę hide, klasa grey tylko pomocniczo nadana
 
+items.forEach((btn)=>{
+    btn.addEventListener('click', (e)=>{
+        const currentItem = e.currentTarget.dataset.info
+        console.log(selected);
+
+        // mapping through data attr and pushig to arr 
+        items.map((item)=>{
+          if(item.dataset.info === currentItem){
+            item.offsetParent.classList.add('grey')
+            selected.push(item.offsetParent)
+          }
+  
+        })
+    })
+    
+})
